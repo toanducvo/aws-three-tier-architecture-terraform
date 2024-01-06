bf6693a203199240f3b8963fc96bcd5248e5ee04
Toan Duc Vo <toan.ducvo@icloud.com>
2024-01-06 20:22:47 +0700

    Fix EBS KMS policy not exist (#8)

--- modules/iam/outputs.tf
+++ modules/iam/outputs.tf
@@ -7,8 +7,3 @@ output "ssm_assume_role_policy" {
     value = data.aws_iam_policy_document.ssm_assume_role_policy.json
     description = "SSM assume role policy"
 }
-
-output "ebs_kms_key_policy" {
-    value = data.aws_iam_policy_document.ebs_kms_key_policy.json
-    description = "KMS policy for encrypting EBS volumes"
-}
