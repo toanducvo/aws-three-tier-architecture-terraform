dbe9c69e5a492f79bf93b4ffc923780da2194124
Toan Duc Vo <toan.ducvo@icloud.com>
2024-01-06 19:39:41 +0700

    Install SSM Agent (#5)

--- envs/dev/scripts/ec2_user_data.sh
+++ envs/dev/scripts/ec2_user_data.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
-#VM Startup Script
+
 sudo yum update -y
-sudo amazon-linux-extras install docker -y
+
+# Install SSM Agent
+sudo yum install -y https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm
+sudo systemctl status amazon-ssm-agent
+sudo systemctl start amazon-ssm-agent
+
+# Install Docker
+sudo yum install -y docker
 sudo systemctl enable docker --now
 sudo systemctl enable containerd.service --now
+
+# Add ec2-user to docker group
 sudo usermod -a -G docker ec2-user
+
+# Pull and run Docker image
 sudo docker pull bjwrd/app.py
 sudo docker run -d bjwrd/app.py 
