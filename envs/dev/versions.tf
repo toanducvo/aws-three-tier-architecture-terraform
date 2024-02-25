75284e2644a14dbbe5eea67d85169f22e36a8239
Toan Duc Vo <toan.ducvo@icloud.com>
2024-02-25 23:47:58 +0700

    chore: upgrade aws version and remove tf required version

--- envs/dev/versions.tf
+++ envs/dev/versions.tf
@@ -1,10 +1,8 @@
 terraform {
-  required_version = "~> 1.6.6"
-
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      version = "~> 4.50.0"
+      version = "~> 5.0"
     }
   }
 }
